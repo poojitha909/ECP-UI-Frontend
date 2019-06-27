3bd5b920a750df50d208ede6f94a00d50ab8de03
khushboo hathiwala <khushboo.hathiwala@gmail.com>
2019-06-27 13:32:17 +0530

    Completed layout component with header and footer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     // component: HomePageComponent
-    loadChildren: () =>
-      import("src/app/features/home/home.module").then(mod => mod.HomeModule)
+    loadChildren: () => import('src/app/features/home/home.module').then((mod) => mod.HomeModule)
   }
 ];
 
--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomePageComponent } from "./home-page/home-page.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LayoutComponent } from 'src/app/ui';
 
 const routes: Routes = [
   {
-    path: "",
-    component: HomePageComponent
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      }
+    ]
   }
 ];
 

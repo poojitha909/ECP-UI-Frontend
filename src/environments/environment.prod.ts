33185e9e3e77d21549fbbff24702c9ec2afc40f6
khushboo hathiwala <khushboo.hathiwala@gmail.com>
2019-08-13 19:06:53 +0530

    UI fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://142.93.215.78/BY/api',
+  apiBaseUrl: 'https://ec.code-alpha.org:8080/BY/api',
   encryptKey: 'SECRET',
   facebook: {
     clientId: '2776633365685417',

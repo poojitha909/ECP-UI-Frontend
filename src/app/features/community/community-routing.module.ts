845b3e480d87b75386fe6be1ca640aaf625a9dcc
Vijay Bhaskar <admin@admins-MacBook-Air.local>
2020-03-30 22:21:27 +0530

    removed search components in engage

--- src/app/features/community/community-routing.module.ts
+++ src/app/features/community/community-routing.module.ts
@@ -9,9 +9,7 @@ import { EventCreatePageComponent } from './components/event-create/event-create
 import { DiscussionDetailPageComponent } from './components/discussion-detail/discussion-detail-page.component';
 import { DiscussionsListPageComponent } from './components/discussions-list/discussions-list-page.component'
 import { DiscussionCreatePageComponent } from './components/discussion-create/discussion-create-page.component'
-import { CommunitySearchResultComponent } from './components/community-search-result/community-search-result.component';
-import { DiscussionSearchListPageComponent } from './components/discussion-search-list-page/discussion-search-list-page.component';
-import { EventSearchListPageComponent } from './components/event-search-list-page/event-search-list-page.component';
+
 const routes: Routes = [
   {
     path: '',
@@ -53,18 +51,6 @@ const routes: Routes = [
   {
     path: 'discussion/:id/:category',
     component: DiscussionDetailPageComponent
-  },
-  {
-    path:'community/search',
-    component:CommunitySearchResultComponent
-  },
-  {
-    path: 'discussion/search',
-    component: DiscussionSearchListPageComponent
-  },
-  {
-    path:'event/search',
-    component:EventSearchListPageComponent
   }
 ];
 

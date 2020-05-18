2b46cd447f100d9058f83f05cb79472a7da677e5
Vijay Bhaskar <vijay.socialalpha.org>
2020-05-18 09:17:29 +0530

    Fixed Share an upcoming event language field

--- src/app/features/community/components/event-create/event-create-page.component.ts
+++ src/app/features/community/components/event-create/event-create-page.component.ts
@@ -65,7 +65,8 @@ export class EventCreatePageComponent implements OnInit {
       capacity:  [event ? event.capacity : "", Validators.required],
       eventType:  [event ? event.eventType : "", Validators.required],
       entryFee:  [event ? event.entryFee : "", Validators.required],
-      languages:  [event ? event.languages : "",[Validators.pattern('^[a-zA-Z \-\']+'),Validators.required]],
+      // languages:  [event ? event.languages : "",[Validators.pattern('^[a-zA-Z \-\']+'),Validators.required]],
+      languages:  [event ? event.languages : "",Validators.required],
       organiser:  [event ? event.organiser : "", [Validators.required,Validators.pattern('^[a-zA-Z \-\']+')]]
     });
   }

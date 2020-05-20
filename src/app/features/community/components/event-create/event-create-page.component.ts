2c0873b6a71ee2969ee9af6d76134fac3ec1ae1a
Vijay Bhaskar <vijay.socialalpha.org>
2020-05-20 10:06:15 +0530

    added api call for language field in event form

--- src/app/features/community/components/event-create/event-create-page.component.ts
+++ src/app/features/community/components/event-create/event-create-page.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from "../../../../core/auth/services/auth.service";
 import { Breadcrumb } from 'src/app/core/interfaces';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import moment from 'moment';
+import { UserService } from 'src/app/features/user/services/user.service';
 
 @Component({
   selector: 'app-event-create',
@@ -32,24 +33,24 @@ export class EventCreatePageComponent implements OnInit {
   eventForm: FormGroup;
   successMessage: string;
   user: any;
-  langList: string[];
+  // langList: string[];
   minDate = moment(new Date()).add(1,'days').format('YYYY-MM-DD')
-  
+  languages:any[];
   constructor(private router: Router, private store: StorageHelperService,
-    private fb: FormBuilder, private authService: AuthService) { }
+    private fb: FormBuilder, private authService: AuthService,private userService:UserService) { }
 
   ngOnInit() {
     document.getElementById("addEventHeading").focus();
     this.categoryList = [];
     this.successMessage = "";
-    this.langList = ["English",
-      "Hindi",
-      "Tamil",
-      "Urdu",
-      "Tulu",
-      "Malyalam",
-      "Greek",
-      "Telugu"];
+    // this.langList = ["English",
+    //   "Hindi",
+    //   "Tamil",
+    //   "Urdu",
+    //   "Tulu",
+    //   "Malyalam",
+    //   "Greek",
+    //   "Telugu"];
     this.user = this.store.retrieve("ECP-USER");
     if (this.user) {
       this.user = JSON.parse(this.user);
@@ -78,10 +79,33 @@ export class EventCreatePageComponent implements OnInit {
       languages:  [event ? event.languages : "",Validators.required],
       organiser:  [event ? event.organiser : "", [Validators.required,Validators.pattern('^[a-zA-Z \-\']+')]]
     });
+    this.userService.profileLanguages().subscribe(response => {
+      if (response) {
+        this.languages = response;
+      }
+    });
   }
 
   get formControl() {
     return this.eventForm.controls;
+  }
+
+
+  addLangFn(name) {
+    return { id: 0, name: name };
+  }
+
+  addLanguage(lang) {
+    if (lang.name) {
+      const language = this.languages.find(value => lang.name.toLowerCase() === value.name.toLowerCase());
+      if (!language) {
+        this.userService.profileLanguages(lang.name).subscribe(response => {
+          if (response) {
+            this.languages = response;
+          }
+        });
+      }
+    }
   }
 
   onReset() {
